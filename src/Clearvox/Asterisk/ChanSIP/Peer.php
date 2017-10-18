0e9f546897e4967e5c573882f10433d7cf6562b9
Joris <joris@clearvox.nl>
2017-10-18 10:35:53 +0200

    spacing

--- src/Clearvox/Asterisk/ChanSIP/Peer.php
+++ src/Clearvox/Asterisk/ChanSIP/Peer.php
@@ -256,8 +256,8 @@ class Peer
     protected $videoSupport;
 
     const VIDEO_SUPPORT_ALWAYS = 'always';
-    const VIDEO_SUPPORT_YES = 'yes';
-    const VIDEO_SUPPORT_NO  = 'no';
+    const VIDEO_SUPPORT_YES    = 'yes';
+    const VIDEO_SUPPORT_NO     = 'no';
 
     /**
      * @var int
